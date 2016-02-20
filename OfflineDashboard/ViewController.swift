b5e846b72782d133648662ae7edbefa749844d56
Max Bhuiyan <mamnun.bd@gmail.com>
2016-02-20 11:28:33 +1100

    Initial Commit

---
+++ OfflineDashboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OfflineDashboard
+//
+//  Created by Mamnun Bhuiyan on 20/02/2016.
+//  Copyright © 2016 IMS Health. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
